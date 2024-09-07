0c5e23603588a88e1e9d762a6786f67941ed028e
Feglawy <a.m9518520963@outlook.com>
2024-09-07 06:17:51 +0300

    Fixed the controller

--- src/Brawlhalla/CodeRedeemer/Controller.py
+++ src/Brawlhalla/CodeRedeemer/Controller.py
@@ -1,38 +1,46 @@
-import keyboard
 import pyautogui
-import win32api
-import win32con
 import time
 from Types import Point
-
+from pynput.keyboard import Controller as KeyboardController, Key, KeyCode
 
 class Controller:
     def __init__(self) -> None:
-        pass
+        self.keyboard = KeyboardController()
 
-    def move_mouse(self, point:Point, duration = 0):
+    def move_mouse(self, point: Point, duration=0):
+        """Moves the mouse to the specified point."""
         pyautogui.moveTo(point.x, point.y, duration=duration)
     
     def click_mouse(self, button='left'):
-        pyautogui.click(button)
+        """Clicks the mouse (left or right)."""
+        pyautogui.click(button=button)
 
-    def __key_down(self, hex_key_code):
-        win32api.keybd_event(hex_key_code, 0, 0, 0)
+    def __key_down(self, key):
+        self.keyboard.press(key)
 
-    def __key_up(self, hex_key_code):
-        win32api.keybd_event(hex_key_code, 0, win32con.KEYEVENTF_KEYUP, 0)
+    def __key_up(self, key):
+        self.keyboard.release(key)
     
-    def press_key(self, hex_key_code):
-        self.__key_down(hex_key_code)
-        time.sleep(0.05)
-        self.__key_up(hex_key_code)
+    def press_key(self, key):
+        """Presses and releases a key."""
+        self.__key_down(key)
+        time.sleep(0.05)  # Small delay to simulate key press
+        self.__key_up(key)
         time.sleep(0.05)
 
     def press_enter(self):
-        self.press_key(0x0D)
-
-    def is_key_pressed(self, key):
-        return keyboard.is_pressed(key)
+        self.press_key(key=Key.enter)
     
+    def select_all_hotkey(self):
+        """Simulate pressing CTRL+A ."""
+        self.__key_down(Key.ctrl)
+        self.__key_down('a')
+        time.sleep(0.05)
+        self.__key_up(Key.ctrl)
+        self.__key_up('a')
+        time.sleep(0.05)
+
+
     def type_text(self, text, interval=0):
+        """Types text using pyautogui with an optional interval."""
         pyautogui.typewrite(text, interval)
